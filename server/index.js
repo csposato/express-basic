1cc9b80a48fb73d936e9be001e644b16b7f86e92
csposato <cristian.sposato@telefonica.com>
2020-08-21 16:38:44 -0300

    Se borro codigo de prueba

--- server/index.js
+++ server/index.js
@@ -6,8 +6,4 @@ server.use(express.json());
 
 server.get('/', (req, res) => res.send('Hello World!'));
 server.use('', routes);
-const a = 5;
-if ((a = 3)) {
-    console.log('no');
-}
 module.exports = server;
